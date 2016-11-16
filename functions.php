5ef4678857efa0d00bd17bb34f2b7d45f9752292
Quincy Ninh <qninh001@seattlecentral.edu>
2016-11-15 21:00:57 -0800

    update sidebar

--- functions.php
+++ functions.php
@@ -6,4 +6,20 @@
   )); 
   add_theme_support('post-thumbnails'); 
   set_post_thumbnail_size(200,200, true);
+
+  add_action( 'widgets_init', 'my_sidebar' );
+  function my_sidebar() {
+    /* Register the 'primary' sidebar. */
+    register_sidebar(
+      array(
+        'id' => 'primary',
+        'name' => __( 'Primary' ),
+        'description' => __( 'This is the primary sidebar.' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>'
+      )
+    );
+  }
 ?>
---
+++ sidebar.php
@@ -0,0 +1,4 @@
+<div id="sidebar-primary" class="sidebar">
+    <br/>
+    <?php dynamic_sidebar( 'primary' ); ?>
+</div>
